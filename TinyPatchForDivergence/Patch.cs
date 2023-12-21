31f02100e022b122fff3980a7990eee61f063a2d
Kaito Kishi <17728957+TRSasasusu@users.noreply.github.com>
2023-12-22 00:58:26 +0900

    fix displayed text as night in mainframe correctly
    Signed-off-by: Kaito Kishi <17728957+TRSasasusu@users.noreply.github.com>

--- TinyPatchForDivergence/Patch.cs
+++ TinyPatchForDivergence/Patch.cs
@@ -9,6 +9,7 @@ using IEnumerator = System.Collections.IEnumerator;
 namespace TinyPatchForDivergence {
     public static class Patch {
         static Coroutine _removeArtificialSun = null;
+        static Coroutine _changeTextOfMainframe = null;
 
         public static void Initialize() {
             LoadManager.OnCompleteSceneLoad += (scene, loadScene) => {
@@ -18,12 +19,18 @@ namespace TinyPatchForDivergence {
                         _removeArtificialSun = null;
                     }
                     _removeArtificialSun = TinyPatchForDivergence.Instance.StartCoroutine(RemoveArtificialSun());
+
+                    if(_changeTextOfMainframe != null) {
+                        TinyPatchForDivergence.Instance.StopCoroutine(_changeTextOfMainframe);
+                        _changeTextOfMainframe = null;
+                    }
+                    _changeTextOfMainframe = TinyPatchForDivergence.Instance.StartCoroutine(ChangeTextOfMainframe());
                 }
             };
         }
 
         static IEnumerator RemoveArtificialSun() {
-            TinyPatchForDivergence.Instance.ModHelper.Console.WriteLine("disalbe artificial sun");
+            TinyPatchForDivergence.Instance.ModHelper.Console.WriteLine("disable artificial sun");
             while(true) {
                 yield return null;
                 var lightsRingInterior = GameObject.Find("Sector_RingInterior/Lights_RingInterior");
@@ -47,6 +54,47 @@ namespace TinyPatchForDivergence {
                     break;
                 }
             }
+        }
+
+        static IEnumerator ChangeTextOfMainframe() {
+            TinyPatchForDivergence.Instance.ModHelper.Console.WriteLine("change text of mainframe");
+            while(true) {
+                yield return null;
+                var dreamWorldBody = GameObject.Find("DreamWorld_Body/Sector_DreamWorld");
+                if(!dreamWorldBody) {
+                    continue;
+                }
+                //TinyPatchForDivergence.Instance.ModHelper.Console.WriteLine("dream world body found");
+                var mainframeCDT = dreamWorldBody.GetComponentsInChildren<CharacterDialogueTree>().FirstOrDefault(x => x._characterName == "Mainframe");
+                if(!mainframeCDT) {
+                    continue;
+                }
+                //TinyPatchForDivergence.Instance.ModHelper.Console.WriteLine("mainframeCDT found");
+                var text = mainframeCDT._xmlCharacterDialogueAsset.text.Replace("DAY", "NIGHT");
+                var textAsset = new TextAsset(text);
+                mainframeCDT.SetTextXml(textAsset);
+                break;
+            }
+            TinyPatchForDivergence.Instance.ModHelper.Console.WriteLine("correctly change text of mainframe");
+
+            foreach (var key in new string[] {
+                "VerifyRingworldARTIFICIAL LIGHTING: OK (STAGE: NIGHT)",
+                "VerifyRingworld_SignalARTIFICIAL LIGHTING: OK (STAGE: NIGHT)",
+            }) {
+                if (TextTranslation.s_theTable.m_language == TextTranslation.Language.JAPANESE) {
+                    TextTranslation.s_theTable.m_table.theTable[key] = "人工太陽:OK(ステージ:夜)";
+                }
+                else {
+                    TextTranslation.s_theTable.m_table.theTable[key] = "ARTIFICIAL LIGHTING: OK (STAGE: NIGHT)";
+                }
+            }
+            TinyPatchForDivergence.Instance.ModHelper.Console.WriteLine("correctly update translation of mainframe");
+            //TinyPatchForDivergence.Instance.ModHelper.Console.WriteLine("look up keys of translation table");
+            //foreach(var key in TextTranslation.s_theTable.m_table.theTable.Keys) {
+            //    if(key.Contains("VerifyRingworld")) {
+            //        TinyPatchForDivergence.Instance.ModHelper.Console.WriteLine($"{key}: {TextTranslation.s_theTable.m_table.theTable[key]}");
+            //    }
+            //}
         }
     }
 }
